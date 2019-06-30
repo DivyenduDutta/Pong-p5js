9d6ceadedb5370e83b70240aeeb3fac988ddec42
divyendu dutta <connect2divyendu@gmail.com>
2019-06-30 11:00:32 +0530

    Added trail to ball

--- ball.js
+++ ball.js
@@ -10,6 +10,8 @@ const BALLORIGINALX = gameConstantsModule.CANVASX/2;
 const BALLORIGINALY = gameConstantsModule.CANVASY/2;
 const BALLRADIUS = 20;
 
+const BALLTRAILLIMIT = 30;
+
 let ballMove, ballTempMove;
 let ballXSpeed = 1; //ball speed x and y is variable because it changes dynamically
 let ballYSpeed = 5;
@@ -21,10 +23,13 @@ let ballHitTop = false;
 let ballHitPlayerPaddle = false;
 let ballHitComPaddle = false;
 
+let ballTrail;
+
 module.exports = {
   BALLORIGINALX, BALLORIGINALY, BALLRADIUS, initBall, moveBall,
   hasBallHitBottom, hasBallHitTop, getBallXDirection,
-  getBallMove, getTempBallMove, setBallMove, setTempBallMove
+  getBallMove, getTempBallMove, setBallMove, setTempBallMove,
+  displayTrail
 }
 
 function initBall(p){
@@ -32,18 +37,21 @@ function initBall(p){
   ballTempMove = p.createVector(0,0);  //used to pause unpause ball
   ballXDirection = p.random([-1, 1]);
   ballYDirection = p.random([-1, 1]);
+  ballTrail = new Array();
 }
 
 function getBallXDirection(){
   return ballXDirection;
 }
 
-function moveBall(playerPaddleMove, comPaddleMove){
+function moveBall(playerPaddleMove, comPaddleMove, p){
   let ballPaddleX = ballMove.x;
   let ballPaddleY = ballMove.y;
   if(gameOverModule.getGameOverFreeze() === false && gameOverModule.getPauseAll() === false){//put this condition in a function check
     ballPaddleX += ballXDirection * ballXSpeed;
     ballPaddleY += ballYDirection * ballYSpeed;
+    populateBallTrail();
+    displayTrail(p);
     if(hasBallHitBottom()){
       ballYDirection = -1;
       ballHitBottom = false;
@@ -69,6 +77,34 @@ function moveBall(playerPaddleMove, comPaddleMove){
     //console.log(ballMove);
     ballMove.set(ballPaddleX, ballPaddleY);
     //console.log(ballMove);
+  }
+}
+
+function populateBallTrail(){
+  //As per this logic the most current position is at the end of the array
+  let currentBallTrailSize = ballTrail.length;
+  let ballPaddleX = ballMove.x;
+  let ballPaddleY = ballMove.y;
+
+  let ballPositionX = BALLORIGINALX + ballPaddleX;
+  let ballPositionY = BALLORIGINALY + ballPaddleY;
+  if(currentBallTrailSize < BALLTRAILLIMIT){
+    ballTrail.push([ballPositionX, ballPositionY]);
+  } else if(currentBallTrailSize === BALLTRAILLIMIT){
+    //remove oldest entry with latest onc
+    ballTrail.shift();
+    ballTrail.push([ballPositionX, ballPositionY]);
+  }
+}
+
+function displayTrail(p){
+  if(ballTrail.length > 10){
+    p.push();
+    ballTrail.forEach(myPoint => {
+      p.fill("grey");
+      p.circle(myPoint[0], myPoint[1],3);
+    });
+    p.pop();
   }
 }
 
